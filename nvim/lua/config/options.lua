be10a1532d91332906d21fcaa908688663210753
38kta-lab <38kta.lab@gmail.com>
2026-05-13 20:02:10 +0900

    feat: ⚡ add molten-nvim + jupytext.nvim (Phase 1: text-only cell execution)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.spell = false
+
+-- Python provider for nvim plugins that need pynvim (molten-nvim 等)。
+-- 専用 conda env `nvim` を作成して pynvim + jupyter_client を入れている。
+-- 他ホスト (Air / mini-lab) でも同 path でセットアップする想定。
+vim.g.python3_host_prog = vim.fn.expand("$HOME/miniforge3/envs/nvim/bin/python3")
